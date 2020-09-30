ae60c5b9c3e7c64d5b9aa655f9d978e80b693c58
Victorhiphub <victor.manuel.rodriguez.candia@uabc.edu.mx>
2020-09-29 21:20:18 -0700

    Coding Problem

---
+++ Practicas/Assets/Scripts/CodingProblem.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CodingProblem : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Hola mundo");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
