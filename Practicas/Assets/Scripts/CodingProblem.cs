36b16bcbc6315ac5a3c6b0f4eed2202d44c7f38d
Victorhiphub <victor.manuel.rodriguez.candia@uabc.edu.mx>
2020-10-02 11:20:08 -0700

    Problema de programacion
    Se creo el codigo al problema de programacion

--- Practicas/Assets/Scripts/CodingProblem.cs
+++ Practicas/Assets/Scripts/CodingProblem.cs
@@ -7,12 +7,26 @@ public class CodingProblem : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("Hola mundo");
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
+        int [] nums={8,1,2,2,3};
         
+        int[] tem = NumeroMenor(nums);
+        for(int i=0;i<nums.Length;i++){
+            Debug.Log("nums: "+ tem[i]);
+        }
+    }
+    int[] NumeroMenor(int[] nums)
+    {
+        int[] temp = new int[nums.Length];
+        int cont=0;
+        for(int i=0;i<nums.Length;i++){
+            cont=0;
+            for(int j=0;j<nums.Length;j++){
+                if(nums[i]>nums[j]){
+                    cont++;
+                }
+            }
+            temp[i]=cont;
+        }
+        return temp;
     }
 }
