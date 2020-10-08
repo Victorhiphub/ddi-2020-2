d16f9947d6d429d4cd4c687038ec4c78eacd5e3f
Victorhiphub <victor.manuel.rodriguez.candia@uabc.edu.mx>
2020-10-07 19:02:10 -0700

    Practica2
    se creo el escenario
    se incorporo al personaje
    se configuro los atributos
    se agrego el panel ui
    se hizo el problema de programacion

---
+++ Practicas/Assets/Scripts/CodingProblem2.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CodingProblem2 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+     string [] nombres ={"Laura","Carlos","Pablo","Junior","Emiliano","Luis","Ignacio","Paola"};
+     string nombre = "Victor";
+     if(lista(nombres,nombre)){
+          Debug.Log("El alumno se encuentra inscrito");
+     }
+     else{
+          Debug.Log("El alumno no se encuentra inscrito");
+     }
+    }
+
+    bool lista(string[] nombres,string nombre){
+        //bool x=null;
+        for(int i=0; i<nombres.Length; i++)
+            if(nombre == nombres[i]){
+                Debug.Log("El alumno es :" +nombres[i]);
+                return true;
+            }
+            return false;
+    }
+
+    
+}
