1a6a83db79f3344d3be2e27959004ff2b44b3e4f
Victorhiphub <victor.manuel.rodriguez.candia@uabc.edu.mx>
2020-11-06 23:42:10 -0800

    Practica 4
    avance hasta la parte 2

---
+++ Practicas/Assets/Scripts/PickUp.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUp : Interactable
+{
+     private Inventory inventory;
+    public Item item;
+    void Start()
+    {
+        inventory = FindObjectOfType<Inventory>();
+        if (inventory == null)
+        {
+            Debug.LogWarning("No se encontró el inventario");
+        }
+    }
+
+    public override void Interact()
+    {
+        Debug.Log("Levantando item");
+        inventory.Add(item);
+        Destroy(gameObject);
+        
+       /* if (item.itemType != ItemType.Money)
+        {
+            inventory.Add(item);
+        }
+        else
+        {
+            Debug.Log("Sumando monedas");
+        }
+        Destroy(gameObject);*/
+    }
+}
