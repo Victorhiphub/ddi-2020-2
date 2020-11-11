b6eaeb93e88cec61665987d058ab708c9d68c4fa
Victorhiphub <victor.manuel.rodriguez.candia@uabc.edu.mx>
2020-11-10 19:34:16 -0800

    Practica 4
    actualizacion

---
+++ Practicas/Assets/Scripts/Donut.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu(fileName = "Nueva Comida", menuName = "Inventario/Food/Chocolate")]
+public class Donut : Item
+{
+    public float recuperacion = 0.9f;
+
+    public int chocolateTiles =10;
+
+    public override void Use()
+    {
+        Debug.Log("Consumiento Dona...se ah recuperado un " + recuperacion + "% De cordura");
+    }
+
+}
