499856b80d53e5c95cbd9ac0ccd549a7e481f5da
Victorhiphub <victor.manuel.rodriguez.candia@uabc.edu.mx>
2020-11-13 21:57:23 -0800

    Practica5

--- Practicas/Assets/Scripts/Interactable.cs
+++ Practicas/Assets/Scripts/Interactable.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
 
 public class Interactable : MonoBehaviour
 {   
@@ -13,7 +14,8 @@ public class Interactable : MonoBehaviour
     }
     void Update()
     {
-        if(isInsideZone && Input.GetKeyDown(KeyCode.E))
+        //Input.GetKeyDown(KeyCode.E)
+        if(isInsideZone && CrossPlatformInputManager.GetButtonDown("Fire1"))
         {
             Interact();
         }
