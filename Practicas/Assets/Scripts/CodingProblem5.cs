d39bade8d38a3a94586894d248e6781d193a00f9
Victorhiphub <victor.manuel.rodriguez.candia@uabc.edu.mx>
2020-11-24 12:24:50 -0800

    practica6

---
+++ Practicas/Assets/Scripts/CodingProblem5.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CodingProblem5 : MonoBehaviour
+{
+    void Start()
+    {
+    int [] num ={12,345,7896,2,6};
+    string nums="";
+    int pairs=0;
+    Debug.Log("Output: "+par(num,pairs,nums));
+    }
+    
+    public int par(int[] num,int pairs,string nums)
+    {
+        int count=0;
+        for(int i=0;i<num.Length;i++)
+        {
+            nums ="" + num[i];
+            for(int j=0; j< nums.Length; j++)
+            {
+                count+=1;
+            }
+            if(count%2==0)
+            {
+                pairs+=1;
+            }
+        count=0;
+        }
+    return pairs;
+
+    }
+}
