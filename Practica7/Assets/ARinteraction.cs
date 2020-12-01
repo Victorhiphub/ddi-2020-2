ae250c316f0b6db2f8bd53161b2607d2cf290090
Victorhiphub <victor.manuel.rodriguez.candia@uabc.edu.mx>
2020-11-30 22:27:55 -0800

    practica7

---
+++ Practica7/Assets/ARinteraction.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ARinteraction : MonoBehaviour
+{
+    public AudioClip Sonido;
+    public float Volumen = 1.5f;
+    protected Transform Posicion = null;
+    ///<sumary>
+    ///OnMouseDown is called when te user has pressed the mouse button while
+    /// over the GUIelement or collider
+    /// <sumary>
+    public void Start()
+    {
+        Posicion=transform;
+    }
+   void OnMouseDown()
+   {
+     AudioSource.PlayClipAtPoint(Sonido,Posicion.position,Volumen);
+       Debug.Log("Comiendo....");
+       Destroy(this.gameObject );
+   }
+
+  
+}
