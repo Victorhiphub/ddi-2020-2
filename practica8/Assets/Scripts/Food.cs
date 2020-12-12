6ae1aed7a7a55808443334ed2b0ee8d6db31e8f5
Victorhiphub <victor.manuel.rodriguez.candia@uabc.edu.mx>
2020-12-11 19:46:28 -0800

    Practica8
    IBM comando de voz

---
+++ practica8/Assets/Scripts/Food.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Food : VoiceInteractable
+{
+
+  
+  
+
+   
+
+    public override void VoiceInteract(string action)
+       {
+          
+        if(action=="hello")
+        {
+        Destroy(gameObject);
+        }
+       
+
+       }
+      
+       
+        
+    }
